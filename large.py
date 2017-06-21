c8e1d1f2c4016f45bbee43e21de3595e78a8c8f8
archu997 <archanagnanam@gmail.com>
2017-06-20 23:32:07 -0700

    Update large.py

--- large.py
+++ large.py
@@ -1,1 +1,9 @@
-
+n=int(input("First No"))
+n1=int(input("Second No"))
+n2=int(input("Third No"))
+if n>n1 and n>n2:
+	print("Largest No:"+str(n))
+elif n1>n2:
+	print("Largest No:"+str(n1))
+else:
+	print("Largest No:"+str(n2))

9dfbb57fd8611de2b7106b288893e2b36b26f8e8
archu997 <archanagnanam@gmail.com>
2017-06-21 21:39:10 -0700

    Update armstrgintrvl.py

--- armstrgintrvl.py
+++ armstrgintrvl.py
@@ -1,11 +1,14 @@
 a1=int(input("First interval"))
 a2=int(input("Second interval"))
-for n in range(a1,a2+1):
+for n1 in range(a1,a2+1):
+    l=len(str(n1))
+    n=int(n1)
     temp=n
     sum=0
     while n>0:
         r=n%10
-        sum=sum+r**3
+        sum=sum+r**l
         n=n/10
     if temp==sum:
         print temp
+

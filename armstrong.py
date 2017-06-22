88c3c08a0257ea3ea004ebd413b8690d5b4d79d8
archu997 <archanagnanam@gmail.com>
2017-06-21 21:39:54 -0700

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,11 @@
-n=int(input("Enter a number"))
+n1=raw_input("Enter a number")
+l=len(n1)
+n=int(n1)
 temp=n
 sum=0
 while n>0:
     r=n%10
-    sum=sum+r**3
+    sum=sum+r**l
     n=n/10
 if temp==sum:
     print "armstrong number"
